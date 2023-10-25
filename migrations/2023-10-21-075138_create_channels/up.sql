e7283f8a7bcbedca12336e0c879e94007b372ea7
DanceMore <dancemore@protonmail.com>
2023-10-24 21:25:50 -0700

    prevent channel1 == channel2 in sql schema :D

--- migrations/2023-10-21-075138_create_channels/up.sql
+++ migrations/2023-10-21-075138_create_channels/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE channel_pairs (
   id SERIAL PRIMARY KEY,
-  channel1 bigint NOT NULL,
+  channel1 bigint NOT NULL CHECK (channel1 <> channel2),
   channel2 bigint NOT NULL,
   CONSTRAINT unique_channel_pair UNIQUE (channel1, channel2)
 );
