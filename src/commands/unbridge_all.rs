ab170e599a71b27478f50c1b88a33cbca57ca112
DanceMore <dancemore@protonmail.com>
2025-04-10 21:50:44 -0700

    checkpoint

--- src/commands/unbridge_all.rs
+++ src/commands/unbridge_all.rs
@@ -10,6 +10,9 @@ use discord_bridgebot::establish_connection;
 use discord_bridgebot::models::ChannelPair;
 use discord_bridgebot::schema::channel_pairs::dsl::*;
 
+use std::num::NonZeroU64;
+use poise::serenity_prelude::ChannelId;
+
 #[poise::command(
     slash_command,
     owners_only,
@@ -54,13 +57,16 @@ pub async fn unbridge_all(ctx: Context<'_>) -> Result<(), Error> {
     }
 
     let mut message = format!(
-        "Attempting to unbridge all connections related to Channel ID `{}`:\n\n",
+        "Attempting to unbridge all connections related to Channel ID `{}`:\nNotifications will be sent to the other side.\n\n",
         current_channel_id
     );
 
     for pair in results {
+        // this logic is ass but it works
+        let mut other_channel = ChannelId::from(NonZeroU64::new(pair.channel2 as u64).unwrap());
         let chan1 = if pair.channel1 == current_channel_id {
-            format!("`{}`!", pair.channel1)
+             other_channel = ChannelId::from(NonZeroU64::new(pair.channel1 as u64).unwrap());
+             format!("`{}`!", pair.channel1)
         } else {
             format!("`{}`", pair.channel1)
         };
@@ -68,16 +74,19 @@ pub async fn unbridge_all(ctx: Context<'_>) -> Result<(), Error> {
         let chan2 = if pair.channel2 == current_channel_id {
             format!("`{}`!", pair.channel2)
         } else {
+            other_channel = ChannelId::from(NonZeroU64::new(pair.channel2 as u64).unwrap());
             format!("`{}`", pair.channel2)
         };
 
         match diesel::delete(channel_pairs.filter(id.eq(pair.id))).execute(connection) {
             Ok(_) => {
                 let emoji = emojis::get_by_shortcode("boom").unwrap();
+                let emoji_warning = emojis::get_by_shortcode("warning").unwrap();
                 message.push_str(&format!(
                     "{} Successfully unbridged Channel ID {} => Channel ID {}\n",
                     emoji, chan1, chan2
                 ));
+                other_channel.say(ctx, format!("{} Guild Owner of Channel ID `{}` initiated an unbridge.", emoji_warning, current_channel_id)).await?;
             }
             Err(_) => {
                 let emoji = emojis::get_by_shortcode("x").unwrap();
